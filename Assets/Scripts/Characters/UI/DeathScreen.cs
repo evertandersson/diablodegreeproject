87fa0650468180fcd3cd11086e1b5b5171412b7b
evertandersson <evert.andersson@edu.futuregames.nu>
2025-01-10 23:19:54 +0100

    fixed inventory restart bug

--- Assets/Scripts/Characters/UI/DeathScreen.cs
+++ Assets/Scripts/Characters/UI/DeathScreen.cs
@@ -1,4 +1,6 @@
 using Game;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -23,11 +25,32 @@ public class DeathScreen : Popup
 
     public void Restart()
     {
-        EventHandler.Main.EventStack.Remove(this);
+        // Create a list to hold events to remove
+        var eventsToRemove = new List<EventHandler.IEvent>();
+
+        // Identify events to remove
+        foreach (var ev in EventHandler.Main.EventStack)
+        {
+            if (ev is not GameManager)
+            {
+                eventsToRemove.Add(ev);
+            }
+        }
+
+        // Remove the identified events from the stack
+        foreach (var ev in eventsToRemove)
+        {
+            EventHandler.Main.EventStack.Remove(ev);
+        }
+
+        activePopups.Clear();
+
+        // Trigger end logic
         OnEnd();
+
+        // Reload the current scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
