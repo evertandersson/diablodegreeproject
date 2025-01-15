8005c4611637240ed4a168d2d758e0bc84deb10b
evertandersson <evert.andersson@edu.futuregames.nu>
2025-01-15 16:02:40 +0100

    .

--- Assets/Scripts/Projectiles/EarthShatter.cs
+++ Assets/Scripts/Projectiles/EarthShatter.cs
@@ -5,8 +5,12 @@ namespace Game
 {
     public class EarthShatter : Fireball
     {
-        public List<Enemy> enemiesHit = new List<Enemy>();
+        public Dictionary<Enemy, int> enemiesHit = new Dictionary<Enemy, int>();
+
         [SerializeField] private ParticleSystem particle;
+
+        private float damageTimer = 0.3f;
+        private float currentTimer = 0f;
 
         private void Awake()
         {
@@ -17,30 +21,40 @@ namespace Game
         {
             base.OnObjectSpawn();
             enemiesHit.Clear();
+            currentTimer = 0;
         }
-
-        protected override void FixedUpdate()
-        {
-            // Do nothing
-        }
-
-        protected override void OnTriggerEnter(Collider other)
-        {
-            // Do nothing
-        }
-
-
 
         private void Update()
         {
             if (particle != null)
             {
+                currentTimer += Time.deltaTime;
+
+                // Deal damage to enemies at intervals
+                if (currentTimer > damageTimer)
+                {
+                    // Create a temporary list of keys to iterate over
+                    var enemiesToProcess = new List<Enemy>(enemiesHit.Keys);
+
+                    foreach (var enemy in enemiesToProcess)
+                    {
+                        if (enemy != null && enemiesHit[enemy] < 2)
+                        {
+                            enemy.TakeDamage(PlayerManager.Instance.Damage);
+                            enemiesHit[enemy] += 1;
+                        }
+                    }
+                    currentTimer = 0; // Reset timer after processing all enemies
+                }
+
+                // Despawn object when particle system is no longer alive
                 if (!particle.IsAlive(true))
                 {
-                    ObjectPooling.Instance.DespawnObject(this.gameObject);
+                    enemiesHit.Clear();
+                    ObjectPooling.Instance.DespawnObject(gameObject);
                 }
             }
         }
-    }
 
+    }
 }
